45be726fa38fb80bc8ecc15d91b5e6db0a4c9b33
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-07-28 04:06:24 +0900

    行列計算

--- main.py
+++ main.py
@@ -79,3 +79,22 @@ y = sigmoid(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
 plt.show()
+#%%
+X = np.array([1, 2])
+W = np.array([[1, 3, 5], [2, 4, 6]])
+Y = np.dot(X, W)
+print(Y)
+
+#%%
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+
+A1 = np.dot(X, W1) + B1
+Z1 = sigmoid(A1)
+A2 = np.dot(Z1, W2) + B2
+Z2 = sigmoid(A2)
+
+print(A2, Z2)
