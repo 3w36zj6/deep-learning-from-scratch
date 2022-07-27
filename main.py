61a34b9346f4d256338126055f47589535bdaa1d
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-07-28 03:05:49 +0900

    xor

--- main.py
+++ main.py
@@ -46,3 +46,14 @@ def NAND(x1, x2):
 for x in [(0, 0), (1, 0), (0, 1), (1, 1)]:
     y = NAND(*x)
     print(f"{x} -> {y}")
+#%%
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+
+for x in [(0, 0), (1, 0), (0, 1), (1, 1)]:
+    y = XOR(*x)
+    print(f"{x} -> {y}")
