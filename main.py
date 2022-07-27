02f71909a8eb88d709011a060207ebdf2d46de6d
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-07-28 03:05:30 +0900

    nand

--- main.py
+++ main.py
@@ -31,3 +31,18 @@ def OR(x1, x2):
 for x in [(0, 0), (1, 0), (0, 1), (1, 1)]:
     y = OR(*x)
     print(f"{x} -> {y}")
+#%%
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+for x in [(0, 0), (1, 0), (0, 1), (1, 1)]:
+    y = NAND(*x)
+    print(f"{x} -> {y}")
