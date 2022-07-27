d19dfeb8d71210322564093335c1424adbb51561
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-07-28 03:05:16 +0900

    or

--- main.py
+++ main.py
@@ -16,3 +16,18 @@ def AND(x1, x2):
 for x in [(0, 0), (1, 0), (0, 1), (1, 1)]:
     y = AND(*x)
     print(f"{x} -> {y}")
+#%%
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+for x in [(0, 0), (1, 0), (0, 1), (1, 1)]:
+    y = OR(*x)
+    print(f"{x} -> {y}")
